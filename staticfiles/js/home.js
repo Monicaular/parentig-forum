0f56f2a0f81dae885ce45537441075226ccfae4a
Monica Ular <moniq.iancu@gmail.com>
2024-04-13 11:46:48 +0000

    Enable static files serving

---
+++ staticfiles/js/home.js
@@ -0,0 +1,11 @@
+let mySwiper = new Swiper('.swiper-container', {
+    
+    direction: 'horizontal',
+    loop: true,
+
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
