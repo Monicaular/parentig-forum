c6492a0ee2cd45a239982555c932ce1c0bb78bd2
Monica Ular <moniq.iancu@gmail.com>
2024-05-19 11:32:24 +0000

    Fix bug in comment and post modal

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const deleteButton = document.querySelector('.delete-btn');
-    const deleteModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    const deletePostModal = new bootstrap.Modal(document.getElementById('deletePostModal'));
+    const deleteCommentModal = new bootstrap.Modal(document.getElementById('deleteCommentModal'));
+    const confirmDeletePost = document.getElementById('confirm-post-delete');
+    const confirmDeleteComm = document.getElementById('confirm-comm-delete');
 
-    deleteButton.addEventListener('click', function() {
-        deleteModal.show();
+    // Delete Post
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete-btn') && !event.target.classList.contains('delete-comm-btn')) {
+            const postId = event.target.getAttribute('post_id');
+            confirmDeletePost.onclick = function() {
+                window.location.href = `delete_post/${postId}`;
+            };
+            deletePostModal.show();
+        }
     });
 
+    //Delete Comment
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('delete-comm-btn')) {
+            const commentId = event.target.getAttribute('comment_id');
+            confirmDeleteComm.onclick = function() {
+                window.location.href = `delete_comment/${commentId}`;
+            };
+            deleteCommentModal.show();
+        }
+    });
+
+   
     window.onload = function() {
         const messages = document.querySelector('.messages');
         if (messages) {
             messages.scrollIntoView({ behavior: 'smooth', block: 'center' });
         }
     };
- });
 
-
-const deleteCommentModal = new bootstrap.Modal(document.getElementById("deleteCommentModal"));
-const confirmDeleteComm = document.getElementById('confirm-delete');
-
- document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete-comm-btn')) {
-        const commentId = event.target.getAttribute('comment_id');
-        confirmDeleteComm.onclick = function() {
-            window.location.href = `delete_comment/${commentId}`;
-        };
-        deleteCommentModal.show();
-    }
+    document.querySelectorAll('.btn-close, .btn-secondary').forEach(function(cancelButton) {
+        cancelButton.addEventListener('click', function() {
+            if (deletePostModal._isShown) {
+                deletePostModal.hide();
+            }
+            if (deleteCommentModal._isShown) {
+                deleteCommentModal.hide();
+            }
+        });
+    });
 });
